ee38e7164b344da24164c8dbf8cbefcbd8530f28
TheLab <>
2020-04-19 17:23:33 +0200

    ADD vga print string

--- src/boot/main.c
+++ src/boot/main.c
@@ -2,17 +2,5 @@
 
 void kmain(){
     init_vga(WHITE, BLACK);
-    print_char('H',WHITE, BLACK);
-    print_char('E',WHITE, BLACK);
-    print_char('L',WHITE, BLACK);
-    print_char('L',WHITE, BLACK);
-    print_char('O',WHITE, BLACK);
-    print_char(' ',WHITE, BLACK);
-    print_char('W',WHITE, BLACK);
-    print_char('O',WHITE, BLACK);
-    print_char('R',WHITE, BLACK);
-    print_char('L',WHITE, BLACK);
-    print_char('D',WHITE, BLACK);
-    print_char('\n',WHITE, BLACK);
-    print_char('A',WHITE, BLACK);
+    print_string("Hello world !",WHITE,BLACK);
 }
